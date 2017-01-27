c4cee1669fafa152a0542894ac6bd0eb38e1ccb7
Pierre Dulac <dulacpier@gmail.com>
2017-01-27 01:12:29 +0100

    Update urls to the new Django 1.10 syntax

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/apps/promotion/urls.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/apps/promotion/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from . import views
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$',
         views.HomeView.as_view(),
         name="home"),
-)
+]
--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/urls.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/urls.py
@@ -1,28 +1,28 @@
 from django.conf import settings
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^', include('apps.promotion.urls', namespace="promotion")),
 
     url(r'^admin/', include(admin.site.urls)),
-)
+]
 
 
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns += (
+    urlpatterns += [
         url(r'^__debug__/', include(debug_toolbar.urls)),
-    )
+    ]
 
 
 if settings.LOCAL_SERVER:
     from django.views.static import serve
-    urlpatterns += patterns('',
+    urlpatterns += [
         url(r'^media/(?P<path>.*)$',
             serve,
             {'document_root': settings.MEDIA_ROOT}),
         # url(r'^404$', TemplateView.as_view(template_name='404.html')),
         # url(r'^500$', TemplateView.as_view(template_name='500.html')),
-    )
+    ]
